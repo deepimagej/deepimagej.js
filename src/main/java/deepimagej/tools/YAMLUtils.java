aae4f6a649ffe69b3baa1148dcdd72f7eff5e4f8
carlosuc3m <49989524+carlosuc3m@users.noreply.github.com>
2020-11-12 02:26:31 +0100

    Update YamlUtils

--- src/main/java/deepimagej/tools/YAMLUtils.java
+++ src/main/java/deepimagej/tools/YAMLUtils.java
@@ -37,32 +37,16 @@
 
 package deepimagej.tools;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 
-import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 
-import deepimagej.DeepImageJ;
-import deepimagej.Parameters;
-import deepimagej.TensorFlowModel;
 import ij.IJ;
 
 public class YAMLUtils {
