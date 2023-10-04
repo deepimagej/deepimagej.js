abbd8ec74ed477ab3547e3dbf3a262a3f96744aa
Wei Ouyang <oeway007@gmail.com>
2023-10-04 16:50:07 +0200

    Add compile action

---
+++ compile-js.sh
@@ -0,0 +1,30 @@
+# This script compiles imagej into image.js
+# It requires CHEERPJ_DIR and IJ_DIR
+set -e
+
+# compile from scratch
+if [ -z ${CHEERPJ_DIR+x} ]
+then
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        curl https://d3415aa6bfa4.leaningtech.com/cheerpj_linux_2.1.tar.gz -LO
+        tar -xvf cheerpj_linux_2.1.tar.gz
+        export CHEERPJ_DIR=$(pwd)/cheerpj_2.1
+    else
+        echo "Please download cheerpj from https://www.leaningtech.com/pages/cheerpj.html#Download and set the CHEERPJ_DIR env variable "
+        exit 1
+    fi
+fi
+
+mvn clean
+mvn install:install-file -Dfile=${CHEERPJ_DIR}/cheerpj-dom.jar -DgroupId=com.learningtech -DartifactId=cheerpj-dom -Dversion=1.0 -Dpackaging=jar
+mvn package
+
+sh get-imagej.sh
+
+IJ_DIR=$(pwd)/dist/ij153
+
+cp target/DeepImageJ_JS_-2.0.1-SNAPSHOT.jar ${IJ_DIR}/plugins
+
+cd ${IJ_DIR}
+# java -jar ij-1.53h.jar
+${CHEERPJ_DIR}/cheerpjfy.py --deps=ij-1.53h.jar -j 4 plugins/DeepImageJ_JS_-2.0.1-SNAPSHOT.jar
